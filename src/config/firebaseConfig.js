cfb602d7226d7d5d65f89185b4525a0d75c51908
Shahi77 <shahiinfinity02@gmail.com>
2023-06-22 13:03:38 +0530

    added authentication and watchlist functionality

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyACO8lmND1umiiCsmd5uiNhUT6EtOcjluE",
+    authDomain: "crypto-hunter-ef804.firebaseapp.com",
+    projectId: "crypto-hunter-ef804",
+    storageBucket: "crypto-hunter-ef804.appspot.com",
+    messagingSenderId: "1022276853960",
+    appId: "1:1022276853960:web:3fde3673b032f8554385fd",
+    measurementId: "G-NDGCGN7CS4"
+};
+
+export default firebaseConfig;
